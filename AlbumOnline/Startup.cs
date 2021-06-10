d1294139fd468cc23ed2ff480a06ab9b7fb6eadd
DELL <DELL@DELL-PC>
2021-06-09 22:21:27 -0600

    Agregar archivos de proyecto.

---
+++ AlbumOnline/Startup.cs
@@ -0,0 +1,87 @@
+﻿using AlbumOnline.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlbumOnline.Startup))]
+namespace AlbumOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRoleUser();
+        }
+
+        void CreateRoleUser()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User     
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "rsolis@unan.edu.ni";
+                user.Email = "rsolis@unan.edu.ni";
+
+                string userPWD = "123456";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role     
+            if (!roleManager.RoleExists("Premium"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Premium";
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role     
+            if (!roleManager.RoleExists("Freemium"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Freemium";
+                roleManager.Create(role);
+                var user = new ApplicationUser();
+                user.UserName = "Freemium@albumonline.com";
+                user.Email = "Freemium@albumonline.com";
+
+                string userPWD = "123456";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Freemium");
+
+                }
+
+            }
+
+        }
+    }
+}
